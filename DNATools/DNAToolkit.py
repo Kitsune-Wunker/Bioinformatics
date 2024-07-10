3d3531e6ae6c947c2bb51120135430cea5f86460
Kitsune-Wunker <yakaikit@gmail.com>
2024-07-10 14:00:15 +0100

    scanning for signs of life...

--- DNATools/DNAToolkit.py
+++ DNATools/DNAToolkit.py
@@ -44,6 +44,7 @@ def gc_content_subset(seq, k=20):
 def translate_seq(seq, init_pos=0):
     return [DNA_Codons[seq[pos:pos + 3]] for pos in range(init_pos, len(seq) - 2, 3)]
 
+# Detects amino acid outputs in DNA
 def codon_usage(seq, aminoacid):
     tmpList = []
     for i in range(0, len(seq) - 2, 3):
@@ -55,3 +56,48 @@ def codon_usage(seq, aminoacid):
     for seq in freqDict:
         freqDict[seq] = round(freqDict[seq] / totalWight, 2)
     return freqDict
+
+# generates all 6 reading frames
+def gen_reading_frames(seq):
+    frames = []
+    frames.append(translate_seq(seq, 0))
+    frames.append(translate_seq(seq, 1))
+    frames.append(translate_seq(seq, 2))
+    frames.append(translate_seq(reverse_complement(seq), 0))
+    frames.append(translate_seq(reverse_complement(seq), 1))
+    frames.append(translate_seq(reverse_complement(seq), 2))
+    return frames
+
+# protein detector
+def proteins_from_rf(aa_seq):
+    current_prot = []
+    proteins = []
+    for aa in aa_seq:
+        if aa == '_':
+            if current_prot:
+                for p in current_prot:
+                    proteins.append(p)
+                current_prot = []
+        else:
+            if aa == "M":
+                current_prot.append("")
+            for i in range(len(current_prot)):
+                current_prot[i] += aa
+    return proteins
+
+# get all proteins from all reading frames
+def all_proteins_from_prfs(seq, startReadPos=0, endReadPos=0, ordered=False):
+    if endReadPos > startReadPos:
+        rfs = gen_reading_frames(seq[startRead: endRead])
+    else:
+        rfs = gen_reading_frames(seq)
+
+    res = []
+    for rf in rfs:
+        prots = proteins_from_rf(rf)
+        for p in prots:
+            res.append(p)
+
+    if ordered:
+        return sorted(res, key=len, reverse=True)
+    return res
--- DNATools/main.py
+++ DNATools/main.py
@@ -4,18 +4,25 @@ import random
 
 # random DNA sequence
 randDNAStr = ''.join([random.choice(Nucleotides)
-                        for nuc in range(51)])
+                        for nuc in range(50)])
 
 DNAStr = validateSeq(randDNAStr)
 #DNAStr = ""
 
-print(f'\nSequence: {DNAStr}\n')
-print(f'[1] + Sequence Length: {len(DNAStr)}\n')
-print(f'[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n')
-print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
-print(f"[4] + DNA + Reverse Complement:\n5' {DNAStr} 3'\n")
-print(f"3' {reverse_complement(DNAStr)} 5'\n")
-print(f"[5] + GC Content: {gc_content(DNAStr)}%\n")
-print(f"[6] + GC Content in subsection k=5: {gc_content_subset(DNAStr, k=5)}\n")
-print(f"[7] + Amino Acid Sequence of DNA: {translate_seq(DNAStr, 0)}\n")
-print(f"[8] + Codon Frequency (L): {codon_usage(DNAStr, 'L')}\n")
+#print(f'\nSequence: {DNAStr}\n')
+#print(f'[1] + Sequence Length: {len(DNAStr)}\n')
+#print(f'[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n')
+#print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
+#print(f"[4] + DNA + Reverse Complement:\n5' {DNAStr} 3'\n")
+#print(f"3' {reverse_complement(DNAStr)} 5'\n")
+#print(f"[5] + GC Content: {gc_content(DNAStr)}%\n")
+#print(f"[6] + GC Content in subsection k=5: {gc_content_subset(DNAStr, k=5)}\n")
+#print(f"[7] + Amino Acid Sequence of DNA: {translate_seq(DNAStr, 0)}\n")
+#print(f"[8] + Codon Frequency (L): {codon_usage(DNAStr, 'L')}\n")
+print(f"[9] + Reading Frames:\n")
+for frame in gen_reading_frames(DNAStr):
+    print(frame)
+
+print('\n[10] + All proteins in 6 open reading frames:')
+for prot in all_proteins_from_prfs(AH002844, 0, 0, True):
+    print(f'{prot}')
